665f111b0c1f7d7374bf71c76149be15a9b0fbea
Dharu65 <98dharu.prasetyo@gmail.com>
2023-12-31 02:37:42 +0700

    first commit

---
+++ src/main/java/com/northwind/service/CustomerService.java
@@ -0,0 +1,36 @@
+package com.northwind.service;
+
+
+import com.northwind.domain.Cart;
+import com.northwind.domain.Customer;
+import com.northwind.repository.CartRepository;
+import com.northwind.repository.CustomerRepository;
+import com.northwind.web.dto.CartDto;
+import com.northwind.web.dto.CustomerDto;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@ApplicationScoped
+@Transactional
+public class CustomerService {
+
+    @Inject
+    CustomerRepository customerRepository;
+
+
+    public static CustomerDto mapToDto(Customer customer) {
+        return new CustomerDto(
+                customer.getCustomerId(),
+                customer.getCompanyName(),
+                customer.getContactName(),
+                customer.getAddress(),
+                customer.getCity(),
+                customer.getCountry()
+        );
+    }
+
+
+}
